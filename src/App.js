0e7226cf06501416c48e1391f7279f6b1ddd1374
KillahBea <bee.noelle6@gmail.com>
2019-05-29 10:05:17 -0400

    trying to set difficulty

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   state = {
     board: [],
     gameId: null,
-    status: []
+    status: [],
+    difficulty: 0
   }
   componentDidMount() {
     const data = { number: 0 }
@@ -29,6 +30,22 @@ class App extends Component {
           gameId: game.id
         })
       })
+  }
+  setDifficulty = event => {
+    this.setState(
+      {
+        difficulty: +event.target.value
+      },
+      () => {
+        const request = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({})
+        }
+      }
+    )
   }
 
   clickBox = (row, column) => {
